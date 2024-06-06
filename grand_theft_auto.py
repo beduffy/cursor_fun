7d5f7d751c3db00857f52e542d431582087d1e80
beduffy <benfduffy@gmail.com>
2024-06-06 21:45:48 +0200

    Driving car in GTA

--- grand_theft_auto.py
+++ grand_theft_auto.py
@@ -24,6 +24,7 @@ player_speed = 5
 car_size = 60
 car_pos = [random.randint(0, screen_width - car_size), random.randint(0, screen_height - car_size)]
 car_speed = 7
+in_car = False
 
 # Bullet settings
 bullet_size = 5
@@ -37,12 +38,15 @@ clock = pygame.time.Clock()
 def draw_player(screen, player_pos):
     pygame.draw.rect(screen, blue, (player_pos[0], player_pos[1], player_size, player_size))
 
+
 def draw_car(screen, car_pos):
     pygame.draw.rect(screen, red, (car_pos[0], car_pos[1], car_size, car_size))
+
 
 def draw_bullets(screen, bullets):
     for bullet in bullets:
         pygame.draw.rect(screen, black, (bullet[0], bullet[1], bullet_size, bullet_size))
+
 
 while not game_over:
     for event in pygame.event.get():
@@ -51,14 +55,36 @@ while not game_over:
 
     keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_LEFT]:
-        player_pos[0] -= player_speed
-    if keys[pygame.K_RIGHT]:
-        player_pos[0] += player_speed
-    if keys[pygame.K_UP]:
-        player_pos[1] -= player_speed
-    if keys[pygame.K_DOWN]:
-        player_pos[1] += player_speed
+    if keys[pygame.K_q]:
+        game_over = True
+
+    if not in_car:
+        if keys[pygame.K_LEFT]:
+            player_pos[0] -= player_speed
+        if keys[pygame.K_RIGHT]:
+            player_pos[0] += player_speed
+        if keys[pygame.K_UP]:
+            player_pos[1] -= player_speed
+        if keys[pygame.K_DOWN]:
+            player_pos[1] += player_speed
+
+        # Check if player enters the car
+        if (car_pos[0] < player_pos[0] < car_pos[0] + car_size or car_pos[0] < player_pos[0] + player_size < car_pos[0] + car_size) and \
+           (car_pos[1] < player_pos[1] < car_pos[1] + car_size or car_pos[1] < player_pos[1] + player_size < car_pos[1] + car_size):
+            if keys[pygame.K_e]:  # Press 'E' to enter the car
+                in_car = True
+                player_speed = car_speed
+
+    else:
+        if keys[pygame.K_LEFT]:
+            car_pos[0] -= car_speed
+        if keys[pygame.K_RIGHT]:
+            car_pos[0] += car_speed
+        if keys[pygame.K_UP]:
+            car_pos[1] -= car_speed
+        if keys[pygame.K_DOWN]:
+            car_pos[1] += car_speed
+        
 
     if keys[pygame.K_SPACE]:
         bullet_pos = [player_pos[0] + player_size // 2, player_pos[1]]
@@ -66,7 +92,8 @@ while not game_over:
 
     screen.fill(white)
 
-    draw_player(screen, player_pos)
+    if not in_car:
+        draw_player(screen, player_pos)
     draw_car(screen, car_pos)
     draw_bullets(screen, bullets)
 
@@ -78,4 +105,4 @@ while not game_over:
     pygame.display.update()
     clock.tick(30)
 
-pygame.quit()xx
+pygame.quit()
